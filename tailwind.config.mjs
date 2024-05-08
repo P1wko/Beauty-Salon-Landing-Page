6199bd9c59eb11ac724510ea3262946bf0a6b1d9
P1wko <miminios20611@gmail.com>
2024-05-08 22:39:45 +0200

    Revert "Merge pull request #4 from AleksandraWojcikk/feature/navbar"
    This reverts commit bfde84462adb5478da67e33a059891ad0683a61e, reversing
    changes made to c8925c6acfacecc51f1cdfb2566a2e83b20d0c1c.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,53 +2,48 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      colors: {
-        pinkLight: '#fbf3f7',
-        accent: '#f55b79',
-        gray: '#c0aeb0',
-        pink: '#f28daf',
-        dark: '#3e2c26',
-        swatch_6: '#de7594',
-        swatch_7: '#a74f46',
-        swatch_8: '#c0956b',
-      },
-    },
-    screens: {
-      'mobile': '1080px',
-    }
-  },
-  plugins: [
-    plugin(function ({ addComponents, theme, addBase }) {
-      const components = {
-        '.title': {
-          fontSize: '30px',
-          fontWeight: 'bold',
-        },
-        '.sectionTitle': {
-          color: theme('colors.accent'),
-          fontWeight: 'bold',
-        },
-        '.accentButton': {
-          background: theme('colors.accent'),
-          color: theme('colors.white'),
-          borderRadius: '10px',
-          padding: '15px 25px',
-        },
-      };
-      addComponents(components);
-      addBase({
-        html: {
-          scrollBehavior: 'smooth',
-        },
-        body: {
-          maxWidth: '1440px',
-          lineHeight: '1.4',
-        },
-      });
-    }),
-    require('tailwind-hamburgers')
-  ],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				pinkLight: '#fbf3f7',
+				accent: '#f55b79',
+				gray: '#c0aeb0',
+				pink: '#f28daf',
+				dark: '#3e2c26',
+				swatch_6: '#de7594',
+				swatch_7: '#a74f46',
+				swatch_8: '#c0956b',
+			},
+		},
+	},
+	plugins: [
+		plugin(function ({ addComponents, theme, addBase }) {
+			const components = {
+				'.title': {
+					fontSize: '30px',
+					fontWeight: 'bold',
+				},
+				'.sectionTitle': {
+					color: theme('colors.accent'),
+					fontWeight: 'bold',
+				},
+				'.accentButton': {
+					background: theme('colors.accent'),
+					color: theme('colors.white'),
+					borderRadius: '10px',
+					padding: '15px 25px',
+				},
+			};
+			addComponents(components);
+			addBase({
+				html: {
+					scrollBehavior: 'smooth',
+				},
+				body: {
+					lineHeight: '1.4',
+				},
+			});
+		}),
+	],
 };
